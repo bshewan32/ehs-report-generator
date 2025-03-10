a1884f782c448d925fbb9e629a0b148f2ecb8887
bshewan32 <198015808+bshewan32@users.noreply.github.com>
2025-03-11 09:43:22 +1100

    commit with production.js

---
+++ config/production.js
@@ -0,0 +1,5 @@
+{
+"MONGODB_URI"="${mongodb+srv://billshewan:qeoCuQjQWwzHlszl@cluster0.xeorh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0}",
+"JWT_SECRET"="${your_jwt_secret_key}",
+"JWT_EXPIRATION"="${604800}"
+}
